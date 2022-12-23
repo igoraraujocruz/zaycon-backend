28e687b89732985bdd3c004d27b61c6c107059e5
Igor <igoraraujocruzz@gmail.com>
2022-12-22 21:20:56 -0300

    changes

--- src/modules/products/services/GetAllByName.ts
+++ src/modules/products/services/GetAllByName.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 import { contract } from '../interfaces/contract';
 import { Product } from '../infra/Entity';
+import { AppError } from '../../../shared/AppError';
 
 @injectable()
 export class GetAllByName {
@@ -11,6 +12,10 @@ export class GetAllByName {
 
     async execute(option: string): Promise<Product[] | undefined> {
         const products = await this.repository.findAllByName(option);
+
+        if(products.length <= 0) {
+            throw new AppError('Nenhum produto encontrado')
+        }
 
         return products;
     }
